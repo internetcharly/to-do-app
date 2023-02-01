486de9eff0903d1123d2978a92d22d5c932724a5
Carlos Agundez <internetcharlytech@gmail.com>
2023-02-01 04:37:19 +0100

    Add tailwind css to the project

--- src/components/InputTask.jsx
+++ src/components/InputTask.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react'
-import '../TaskCard.css'
 
 export function InputTask({ tasks, setTasks }) {
 	const [idCounter, setIdCounter] = useState(0)
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import { useEffect } from 'react'
-import '../TaskCard.css'
 
 export function TaskCard({ tasks, setTasks }) {
 	const deleteTask = (id) => {
